fd869e68f26b021d098055a3567954980c6a3746
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-09 05:18:00 +0530

    Create string_variable_play.py
    We have added a string which is inside a variable and it should be added here for simple play nothing fancy

---
+++ string_variable_play.py
@@ -0,0 +1,9 @@
+# how to play with strings in python
+
+# we will add a name variable in python such that it should be name defined
+
+name = 'youtube'
+
+# now we will print one string which is inside the variable and one string that is inside  the name
+
+print(name+' rocks')
