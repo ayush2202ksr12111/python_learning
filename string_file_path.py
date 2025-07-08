2ea08ef7293513ba8d98c0cec8bbf42982c99c9d
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-09 04:48:40 +0530

    Create string_file_path.py
    This is a string file path which need to be stored.

---
+++ string_file_path.py
@@ -0,0 +1,5 @@
+# Suppose I have a string which contain the file path how will it print in python
+
+file_path = 'c:\docs\naveen'
+# this will print the in some different format which is not as you have thought of
+print(file_path)
