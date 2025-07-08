6789cdecdd1e9bbb0da5badde7bddb6ed5d713ca
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-09 05:00:24 +0530

    Create integer_dynamics_variable.py
    We have added an integer dynamic variable here

---
+++ integer_dynamics_variable.py
@@ -0,0 +1,11 @@
+# variables are container that will store your values
+
+# we play the add game 
+
+# we have defined an x variable
+
+x=2
+# we have added the 3 directly
+print(x+3)
+
+#_____________________________________________
