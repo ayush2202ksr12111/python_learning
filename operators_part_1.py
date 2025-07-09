b81dac5f52c14dfd5090dd6b135a3d5f8873743a
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-10 03:19:32 +0530

    Create operators_part_1.py

---
+++ operators_part_1.py
@@ -0,0 +1,29 @@
+# we are writing operators in python 
+# we are declaring variable x as 2
+
+x=2
+
+# we are declaring variable y as 3
+
+y=3
+
+# now we are going to add the variable 
+
+
+x = x+2
+
+print(x)
+# another way to adding a variable here shortcut way
+
+x+=2
+print(x)
+# now we are going to multiply the variable
+
+x=x*2
+print(x)
+
+# now we are going to multiply suing short cut method
+
+x*=2
+print(x)
+
