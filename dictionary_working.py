c125fdeef5abb037e3401b29bf9bf14e4480fe38
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-11 03:26:23 +0530

    Create dictionary_working.py

---
+++ dictionary_working.py
@@ -0,0 +1,7 @@
+# dictionary define using key pair method where we can find the number using the key value to determine pair value
+
+d = {'navin':'samsung','rahul':'iphone','kiran':'oneplus'}
+
+# this method will display all the keys that are necessary to display just only keys
+
+print(d.keys())
