26b8758bae56b699366c25b94e265326ff817dce
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-11 03:16:15 +0530

    Create tuple_woorking.py
    This will display how tuple works

---
+++ tuple_woorking.py
@@ -0,0 +1,14 @@
+#TUPLE 
+
+# List  can contain integer or float or string
+#list is mutable or changable
+
+# meanwhile tuple is immutable which is non changable 
+
+# tupple contain floowing variables which can be set of integers
+
+tup = (12, 36, 14, 25)
+
+# this will display the first index position of integer
+
+print(tup[1])
